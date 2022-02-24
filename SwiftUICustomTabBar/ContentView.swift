72b449184b600c999287acd59940db0de2d42e1f
HengJay <hengjay.wang@itri.org.tw>
2022-02-24 21:14:20 +0800

    Initial Commit

---
+++ SwiftUICustomTabBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICustomTabBar
+//
+//  Created by M200_Macbook_Pro on 2022/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICustomTabBar/SwiftUICustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICustomTabBarApp.swift
+//  SwiftUICustomTabBar
+//
+//  Created by M200_Macbook_Pro on 2022/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
